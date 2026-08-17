2dc3d5102d5316cac52f2b29e4985a9b6d850754
lsv <ls7777@yandex.ru>
2026-08-17 16:53:34 +0500

    Minor changes for the operation of a non‑privileged user.
    Некоторые улучшения для возможности работы ограниченных пользователей.
    (permission denied for table pg_proc)

--- schema/pgSchema.cpp
+++ schema/pgSchema.cpp
@@ -491,7 +491,7 @@ pgObject *pgSchemaBaseFactory::CreateObjects(pgCollection *collection, ctlTree *
 	restr += wxT("((nspname = 'pg_catalog' AND EXISTS (SELECT 1 FROM pg_class WHERE relname = 'pg_class' AND relnamespace = nsp.oid LIMIT 1)) OR\n");
 	restr += wxT("(nspname = 'pgagent' AND EXISTS (SELECT 1 FROM pg_class WHERE relname = 'pga_job' AND relnamespace = nsp.oid LIMIT 1)) OR\n");
 	restr += wxT("(nspname = 'information_schema' AND EXISTS (SELECT 1 FROM pg_class WHERE relname = 'tables' AND relnamespace = nsp.oid LIMIT 1)) OR\n");
-	restr += wxT("(nspname LIKE '_%' AND EXISTS (SELECT 1 FROM pg_proc WHERE proname='slonyversion' AND pronamespace = nsp.oid LIMIT 1))\n");
+	restr += wxT("(nspname LIKE '_%' AND EXISTS (SELECT 1 FROM information_schema.routine_privileges f where f.routine_name='slonyversion' and f.grantee=current_user and f.routine_schema=nspname LIMIT 1))\n");
 
 	// We add the below schemas at initdb time itself. So users cannot create
 	// schemas of the same name later. However if DB is started in PostgreSQL
