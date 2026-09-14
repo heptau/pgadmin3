d7e1132cd4bd669e215c8ad582294e29ed92f130
lsv <ls7777@yandex.ru>
2026-09-14 17:45:28 +0500

    docs: Prepare release
    Исправлены инструкции. Изменена версия pgadmin3 на 1.27.0.
    Создан changelog.md из содержимого коммитов.
    Добавлена инструкция для инсталяции в linux.

--- include/copyright.h
+++ include/copyright.h
@@ -12,8 +12,8 @@
 #ifndef COPYRIGHT_H
 #define COPYRIGHT_H
 
-#define COPYRIGHT               wxT("Copyright 2002 - 2016, The pgAdmin Development Team")
-#define COPYRIGHT_WIN32         "\251 2002 - 2016, The pgAdmin Development Team"
+#define COPYRIGHT               wxT("Copyright 2018 - 2026, Fork of pgAdmin by levinsv")
+#define COPYRIGHT_WIN32         "\251 2018 - 2026, Fork of pgAdmin by levinsv"
 #define LICENSE                 _("This software is released under the PostgreSQL Licence.")
 
 #endif
